ddc7345f0cee2983c594635a5e676a4f27e43f4e
Kristina Shandrenko <kristinashandrenko@MacBook-Pro-Kristina.local>
2016-02-02 23:11:19 +0200

    applied formatter

--- src/test/java/org/sergii/cucumber/Launcher.java
+++ src/test/java/org/sergii/cucumber/Launcher.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty"}, features = {"src/test/resources/feature/my.feature"})
+@CucumberOptions(format = {"junit:cucumber/cucumber.xml"}, features = {"src/test/resources/feature/my.feature"})
 public class Launcher {
 }
